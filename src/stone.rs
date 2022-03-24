6bc2665e641095e13ae500ac28b31ff755a8f5be
Linus Weigand <linusweigand@protonmail.com>
2022-03-24 22:53:43 +0100

    Block masks

--- src/stone.rs
+++ src/stone.rs
@@ -21,4 +21,57 @@ impl Stone {
             color
         }
     }
+
+    pub fn l(x: usize, y: usize, color: String) -> Stone {
+        Stone {
+            x, y,
+            block_mask: [
+                [false, false, false, false],
+                [false, true, false, false],
+                [false, true, false, false],
+                [false, true, true, false],
+            ],
+            color
+        }
+    }
+
+    pub fn square(x: usize, y: usize, color: String) -> Stone {
+        Stone {
+            x, y,
+            block_mask: [
+                [false, false, false, false],
+                [false, false, false, false],
+                [false, true, true, false],
+                [false, true, true, false],
+            ],
+            color
+        }
+    }
+
+    pub fn rev_l(x: usize, y: usize, color: String) -> Stone {
+        Stone {
+            x, y,
+            block_mask: [
+                [false, false, false, false],
+                [false, false, true, false],
+                [false, false, true, false],
+                [false, true, true, false],
+            ],
+            color
+        }
+    }
+
+    pub fn triangle(x: usize, y: usize, color: String) -> Stone {
+        Stone {
+            x, y,
+            block_mask: [
+                [false, false, false, false],
+                [false, false, false, false],
+                [false, true, false, false],
+                [true, true, true, false],
+            ],
+            color
+        }
+    }
+
 }
